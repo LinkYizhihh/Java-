b2d83d704a7d31aecc34abdd98ba15af03a97942
LinkYizhihh <87404078+LinkYizhihh@users.noreply.github.com>
2021-09-24 17:01:03 +0800

    Create practice13.java
    已知有一个数列：f(0) = 1,f(1) = 4,f(n+2)=2*f(n+1) + f(n),其中n是大于0
    的整数，求f(10)的值。

---
+++ 2021-9/practice13.java
@@ -0,0 +1,21 @@
+package jiuYueErShiSi;
+
+public class Practice2 {
+	public static void main(String[] args) {
+		int n=10;
+		Practice2 practice2=new Practice2();
+		int result=practice2.f(n);
+		System.out.print(result);
+	}
+	//递归算法
+	public int f(int n) {
+		if(n>1){
+			return 2*f(n-1)+f(n-2);
+		}else if(n==21){
+			return 4;
+		}else if(n==20){
+			return 1;
+		}
+	}
+
+}
